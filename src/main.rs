5ca196826cc1fec060f82608bb283fb24c3eabcc
Jonathan Lu <jonathan.ty.lu@gmail.com>
2024-09-23 18:57:48 -0700

    src/main: can parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::env;
+
+// Print the usage message and quit the process
+fn print_usage_and_exit(program_name: &str) {
+    eprintln!("Usage: {program_name} [-n LEN] FILE");
+    std::process::exit(1);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Read in command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    let program_name = &args[0];
+    let mut len: Option<usize> = None;
+    let mut file_path: Option<String> = None;
+
+    // loop through command line arguments to check for optional flags
+    let mut i = 1;
+    while i < args.len() {
+        // match the argument with either the "-n" option or FILE
+        match args[i].as_str() {
+            "-n" => {
+                i += 1;
+                // check if length exists, and if it is valid
+                if i < args.len() {
+                    len = Some(args[i].parse().unwrap_or_else(|error| {
+                        panic!("Invalid length: {}", args[i]);
+                    }));
+                } else {
+                    print_usage_and_exit(program_name);
+                }
+            }
+            _ => {
+                if file_path.is_none() {
+                    file_path = Some(args[i].clone());
+                } else {
+                    print_usage_and_exit(program_name);
+                }
+            }
+        }
+        i += 1
+    }
+
+
 }
