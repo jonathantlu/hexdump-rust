e9dd8f85f273120a6c9e2c01838c089701e1e743
Jonathan Lu <jonathan.ty.lu@gmail.com>
2024-09-23 21:23:56 -0700

    added unit tests for argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ use std::fs;
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(args.file_path)?;
 
-    println!("{contents}");
-
     Ok(())
 }
 
@@ -19,7 +17,7 @@ impl Args {
         format!("Usage: {program_name} [-n LEN] FILE")
     }
 
-    pub fn build(args: &[String]) -> Result<Args, String> {
+    pub fn parse(args: &[String]) -> Result<Args, String> {
         let name = &args[0];
         let mut len: Option<usize> = None;
         let mut file_path: Option<String> = None;
@@ -65,5 +63,120 @@ impl Args {
         };
 
         Ok(Args { len, file_path })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_args() {
+        let args = vec![String::from("./hexdump")];
+        match Args::parse(&args) {
+            Err(e) => {
+                assert_eq!(e.as_str(), "Usage: ./hexdump [-n LEN] FILE");
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+
+    // arg parse testing
+    #[test]
+    fn working_args() {
+        let args = vec![String::from("./hexdump"), String::from("haiku.txt")];
+        let args = Args::parse(&args).unwrap();
+        assert_eq!(args.len, None);
+        assert_eq!(args.file_path.as_str(), "haiku.txt");
+    }
+
+    #[test]
+    fn working_args2() {
+        let args = vec![String::from("./hexdump"), String::from("haiku.txt"), String::from("hello")];
+        let args = Args::parse(&args).unwrap();
+        assert_eq!(args.len, None);
+        assert_eq!(args.file_path.as_str(), "haiku.txt");
+    }
+
+    #[test]
+    fn working_args_with_optional_flag() {
+        let args = vec![String::from("./hexdump"), String::from("-n"), String::from("1"), String::from("haiku.txt")];
+        let args = Args::parse(&args).unwrap();
+        assert_eq!(args.file_path.as_str(), "haiku.txt");
+        assert_eq!(1, args.len.unwrap());
+    }
+
+    #[test]
+    fn working_args_with_optional_flag2() {
+        let args = vec![String::from("./hexdump"), String::from("haiku.txt"), String::from("-n"), String::from("1")];
+        let args = Args::parse(&args).unwrap();
+        assert_eq!(args.file_path.as_str(), "haiku.txt");
+        assert_eq!(1, args.len.unwrap());
+    }
+
+    #[test]
+    fn working_args_with_optional_flag3() {
+        let args = vec![String::from("./hexdump"), String::from("haiku.txt"), String::from("-n"), String::from("1"), String::from("aaaaa")];
+        let args = Args::parse(&args).unwrap();
+        assert_eq!(args.file_path.as_str(), "haiku.txt");
+        assert_eq!(1, args.len.unwrap());
+    }
+
+    #[test]
+    fn broken_args_with_flag() {
+        let args = vec![String::from("./hexdump"), String::from("haiku.txt"), String::from("-n")];
+        let args = Args::parse(&args);
+        match args {
+            Err(e) => {
+                assert_eq!(e.as_str(), "Usage: ./hexdump [-n LEN] FILE");
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+
+    #[test]
+    fn broken_args_with_flag2() {
+        let args = vec![String::from("./hexdump"), String::from("haiku.txt"), String::from("-n"), String::from("r")];
+        let args = Args::parse(&args);
+        match args {
+            Err(e) => {
+                assert_eq!(e.as_str(), "Invalid length: r\nUsage: ./hexdump [-n LEN] FILE");
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+
+    #[test]
+    fn broken_args_with_flag3() {
+        let args = vec![String::from("./hexdump"), String::from("-n"), String::from("r")];
+        let args = Args::parse(&args);
+        match args {
+            Err(e) => {
+                assert_eq!(e.as_str(), "Invalid length: r\nUsage: ./hexdump [-n LEN] FILE");
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+
+    #[test]
+    fn args_with_no_file() {
+        let args = vec![String::from("./hexdump"), String::from("-n"), String::from("4")];
+        let args = Args::parse(&args);
+        match args {
+            Err(e) => {
+                assert_eq!(e.as_str(), "Usage: ./hexdump [-n LEN] FILE");
+            }
+            _ => {
+                panic!();
+            }
+        }
     }
 }
